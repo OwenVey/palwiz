ba768d8556fb48f892c3e4c0a8ccf7543bf26169
Owen Vey <owen.vey@gmail.com>
2024-01-26 08:03:34 -0600

    add in navbar

---
+++ src/components/Map.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L, { LatLngBoundsExpression, LatLngExpression } from "leaflet";
+
+const Map = () => {
+  const bounds = [
+    [0, 0],
+    [1000, 1000],
+  ] satisfies LatLngBoundsExpression;
+  const MAX_ZOOM = 5;
+
+  return (
+    <MapContainer
+      crs={L.CRS.Simple}
+      zoom={1}
+      worldCopyJump={true}
+      maxBounds={bounds}
+      style={{ height: "100vh", width: "100%" }}
+      maxZoom={MAX_ZOOM}
+    >
+      {/* Replace with your own custom tile */}
+      <TileLayer
+        url="http://localhost:1234/tiles/{z}/{x}/{y}.png"
+        noWrap={true}
+        bounds={bounds}
+        maxZoom={MAX_ZOOM}
+      />
+    </MapContainer>
+  );
+};
+
+export default Map;
