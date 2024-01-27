4526f96de41e59a5079fb9d74a1f72facb09d3e1
Owen Vey <owen.vey@gmail.com>
2024-01-27 16:56:18 -0600

    wrap pals grid in suspense boundry

--- src/app/pals/page.tsx
+++ src/app/pals/page.tsx
@@ -1,12 +1,15 @@
 import PalsGrid from '@/app/pals/grid';
 import pals from '@/data/pals.json';
+import { Suspense } from 'react';
 
 export default function PalsPage() {
   const sortedPals = pals.sort((a, b) => a.zukanIndex - b.zukanIndex);
 
   return (
     <div className="mt-4">
-      <PalsGrid pals={sortedPals} />
+      <Suspense>
+        <PalsGrid pals={sortedPals} />
+      </Suspense>
     </div>
   );
 }
