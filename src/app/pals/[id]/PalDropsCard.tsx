e85b492d000b6d55d8946d2d3201e023723561da
Owen Vey <owen.vey@gmail.com>
2024-02-02 20:34:48 -0600

    format

--- src/app/pals/[id]/PalDropsCard.tsx
+++ src/app/pals/[id]/PalDropsCard.tsx
@@ -50,7 +50,7 @@ export function ItemDropsCard({ drops, alphaDrops, className, ...rest }: ItemDro
           </div>
         </CardTitle>
       </CardHeader>
-      <div className="@xs:grid-cols-2 @lg:grid-cols-3 @2xl:grid-cols-4 grid grid-cols-1 gap-2">
+      <div className="grid grid-cols-1 gap-2 @xs:grid-cols-2 @lg:grid-cols-3 @2xl:grid-cols-4">
         {dropsToShow.map((drop) => (
           <Tooltip key={drop.id}>
             <TooltipTrigger asChild>
