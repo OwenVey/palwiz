8dac86db3eba22c357665337cd7ed1688ee58467
Owen Vey <owen.vey@gmail.com>
2024-02-07 20:11:18 -0600

    fix slow search bug in pal grid

--- src/app/pals/pals-grid.tsx
+++ src/app/pals/pals-grid.tsx
@@ -13,12 +13,13 @@ import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/u
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { PAL_ELEMENTS, WORK_SUITABILITIES } from '@/constants';
 import { normalPals } from '@/data/parsed';
-import { isWithinRange, notEmpty, sortArrayByPropertyInDirection } from '@/lib/utils';
+import { isWithinRange, notEmpty, parseAsArrayOfStrings, sortArrayByPropertyInDirection } from '@/lib/utils';
 import { type Pal, type WorkSuitability } from '@/types';
+import { useDebounce } from '@uidotdev/usehooks';
 import { ArrowDownNarrowWideIcon, ArrowDownWideNarrowIcon, ArrowUpDownIcon, GemIcon, SearchIcon } from 'lucide-react';
 import Link from 'next/link';
-import { parseAsArrayOf, parseAsString, parseAsStringLiteral, useQueryState } from 'nuqs';
-import { useMemo } from 'react';
+import { parseAsString, parseAsStringLiteral, useQueryState } from 'nuqs';
+import { memo, useMemo } from 'react';
 
 export const PAL_SORTS = [
   { label: 'Capture Rate', value: 'captureRateCorrect' },
@@ -54,14 +55,16 @@ export function PalsGrid() {
   );
   const [rarity, setRarity] = useQueryState('rarity', parseAsString.withDefault(''));
   const [work, setWork] = useQueryState('work', parseAsStringLiteral(WORK_SUITABILITIES.map(({ id }) => id)));
-  const [elements, setElements] = useQueryState('elements', parseAsArrayOf(parseAsString).withDefault([]));
+  const [elements, setElements] = useQueryState('elements', parseAsArrayOfStrings);
   const [partnerSkill, setPartnerSkill] = useQueryState('partnerSkill');
+
+  const debouncedSearch = useDebounce(search, 100);
 
   const filteredPals = useMemo(() => {
     return sortArrayByPropertyInDirection(
       normalPals.filter((pal) => {
         if (pal.zukanIndex <= 0 || pal.isBoss) return false;
-        if (search && !pal.name.toLowerCase().includes(search.toLowerCase())) return false;
+        if (debouncedSearch && !pal.name.toLowerCase().includes(debouncedSearch.toLowerCase())) return false;
         if (rarity && !isCorrectRarity(rarity ?? 'all', pal.rarity)) return false;
         if (elements.length && ![pal.elementType1, pal.elementType2].some((e) => e && elements.includes(e)))
           return false;
@@ -72,7 +75,7 @@ export function PalsGrid() {
       sort,
       sortDirection,
     ).sort((pal1, pal2) => (work ? pal2.workSuitabilities[work] - pal1.workSuitabilities[work] : 0));
-  }, [elements, partnerSkill, rarity, search, sort, sortDirection, work]);
+  }, [elements, partnerSkill, rarity, debouncedSearch, sort, sortDirection, work]);
 
   function isCorrectRarity(rarityGroup: string, rarity: number) {
     if (rarityGroup === 'all') return true;
@@ -210,57 +213,65 @@ export function PalsGrid() {
         </div>
       </Card>
 
-      <div className="flex-1 @container">
-        {filteredPals.length > 0 ? (
-          <div className="grid grid-cols-1 gap-4 @sm:grid-cols-2 @xl:grid-cols-3 @3xl:grid-cols-4 @5xl:grid-cols-5">
-            {filteredPals.map((pal) => (
-              <Link href={`/pals/${pal.id}`} key={pal.id}>
-                <Card className="relative p-2" hoverEffect>
-                  {sort !== 'name' && sort !== 'zukanIndex' && (
-                    <Badge variant="primary" className="absolute -right-1 -top-1 z-10">
-                      {pal[sort].toLocaleString()}
-                    </Badge>
-                  )}
-                  <div className="relative flex w-full justify-between">
-                    <div className="absolute left-0 flex flex-col">
-                      <Badge className="h-fit items-baseline">
-                        <span className="text-gray-8">#{'000'.slice(pal.zukanIndex.toString().length)}</span>
-                        <span>{pal.zukanIndex}</span>
-                        <span className="text-[10px]">{pal.zukanIndexSuffix}</span>
-                      </Badge>
-
-                      <div className="mt-2 flex flex-col gap-1">
-                        {[pal.elementType1, pal.elementType2].filter(Boolean).map((element) => (
-                          <ElementImage key={element} element={element} />
-                        ))}
-                      </div>
-                    </div>
-
-                    <div className="absolute right-0 flex flex-col">
-                      {Object.entries(pal.workSuitabilities)
-                        .filter(([, value]) => value > 0)
-                        .sort(([, value1], [, value2]) => value2 - value1)
-                        .map(([work, value]) => (
-                          <div key={work} className="flex items-center">
-                            <WorkTypeImage id={work} />
-                            <span className="text-xs font-semibold text-gray-11">{value}</span>
-                          </div>
-                        ))}
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col items-center gap-3 py-2">
-                    <PalImage id={pal.id} className="rounded-full border border-gray-6 bg-gray-1" />
-                    <div className="font-medium">{pal.name}</div>
-                  </div>
-                </Card>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <div className="grid h-full place-items-center text-gray-11">No pals found</div>
-        )}
-      </div>
+      <Grid pals={filteredPals} sort={sort} />
     </div>
   );
 }
+
+const Grid = memo(function Grid({ pals, sort }: { pals: Pal[]; sort: keyof Pal }) {
+  console.log('[GRID]');
+
+  return (
+    <div className="flex-1 @container">
+      {pals.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 @sm:grid-cols-2 @xl:grid-cols-3 @3xl:grid-cols-4 @5xl:grid-cols-5">
+          {pals.map((pal) => (
+            <Link href={`/pals/${pal.id}`} key={pal.id}>
+              <Card className="relative p-2" hoverEffect>
+                {sort !== 'name' && sort !== 'zukanIndex' && (
+                  <Badge variant="primary" className="absolute -right-1 -top-1 z-10">
+                    {pal[sort]!.toLocaleString()}
+                  </Badge>
+                )}
+                <div className="relative flex w-full justify-between">
+                  <div className="absolute left-0 flex flex-col">
+                    <Badge className="h-fit items-baseline">
+                      <span className="text-gray-8">#{'000'.slice(pal.zukanIndex.toString().length)}</span>
+                      <span>{pal.zukanIndex}</span>
+                      <span className="text-[10px]">{pal.zukanIndexSuffix}</span>
+                    </Badge>
+
+                    <div className="mt-2 flex flex-col gap-1">
+                      {[pal.elementType1, pal.elementType2].filter(Boolean).map((element) => (
+                        <ElementImage key={element} element={element} />
+                      ))}
+                    </div>
+                  </div>
+
+                  <div className="absolute right-0 flex flex-col">
+                    {Object.entries(pal.workSuitabilities)
+                      .filter(([, value]) => value > 0)
+                      .sort(([, value1], [, value2]) => value2 - value1)
+                      .map(([work, value]) => (
+                        <div key={work} className="flex items-center">
+                          <WorkTypeImage id={work} />
+                          <span className="text-xs font-semibold text-gray-11">{value}</span>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center gap-3 py-2">
+                  <PalImage id={pal.id} className="rounded-full border border-gray-6 bg-gray-1" />
+                  <div className="font-medium">{pal.name}</div>
+                </div>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="grid h-full place-items-center text-gray-11">No pals found</div>
+      )}
+    </div>
+  );
+});
