5b3d26f07bae9ceacd1679cd62145921adc79118
Owen Vey <owen.vey@gmail.com>
2024-02-15 09:18:50 -0600

    add blue bg to map loader

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -12,7 +12,7 @@ export default function MyPage() {
     () =>
       dynamic(() => import('@/components/Map'), {
         loading: () => (
-          <div className="fixed grid h-screen w-screen place-items-center">
+          <div className="fixed grid h-screen w-screen place-items-center bg-[#102536]">
             <Spinner className="size-6" />
           </div>
         ),
