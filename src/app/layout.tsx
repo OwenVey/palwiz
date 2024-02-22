bd3e604f7499d9933578deeab36a63d6b3060c91
Owen Vey <owen.vey@gmail.com>
2024-02-22 17:41:25 -0600

    add overflow-y-scroll to html to prevent layout shift from scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="overflow-y-scroll" suppressHydrationWarning>
       <body className={cn(inter.variable, GeistMono.variable, 'bg-gray-1 font-sans antialiased')}>
         <Providers>
           <Navbar />
