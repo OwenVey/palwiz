5c8df4bdcee2c282773093eee96c775523bff94f
Owen Vey <owen.vey@gmail.com>
2024-02-22 18:37:27 -0600

    add padding on right side of input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -29,8 +29,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             value={value}
             onChange={onChange}
             className={cn(
-              Icon ? 'pl-10 pr-3' : 'px-3',
-              'flex h-10 w-full rounded-lg border border-gray-6 bg-gray-1 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-10 hover:border-gray-7 focus-visible:border-primary-9 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-4 disabled:cursor-not-allowed disabled:opacity-50',
+              Icon ? 'pl-10' : 'pl-3',
+              'flex h-10 w-full truncate rounded-lg border border-gray-6 bg-gray-1 py-2 pr-9 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-10 hover:border-gray-7 focus-visible:border-primary-9 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-4 disabled:cursor-not-allowed disabled:opacity-50',
             )}
             ref={ref}
             {...props}
